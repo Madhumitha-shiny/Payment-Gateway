570cf2b85e7d690ce34904f78e02076f13ff4d09
MadhumithaShiny <smshiny.12@gmail.com>
2022-06-15 10:33:35 +0530

    Edits completed

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ export default function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/donate" element={<Donate />} />
+          <Route path="*"  element={<Home /> } />
         </Route>
       </Routes>
       <Footer />
